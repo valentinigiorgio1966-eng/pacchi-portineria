8f35c46d29ffb6a575ccacc00c9c346f2c36eb9e
valentinigiorgio1966-eng <valentinigiorgio1966@gmail.com>
2026-03-04 12:14:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE="pacchi-20260304104340";
+const CACHE="pacchi-v2";
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./logo.png"])))});
 
 self.addEventListener("fetch",e=>{
